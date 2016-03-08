b646abdbe663a48d57dc7d08e9d2c72da3b7ef84
leikaifeng <lkf672433945@163.com>
2016-03-08 16:32:59 +0800

    people.cpp
    description of people class

---
+++ people.cpp
@@ -0,0 +1,20 @@
+class people{
+  public:
+    int age();
+    double height();
+    string sex();
+  private:
+    string name;
+    int sex;  // 1 for male, 0 for female
+    int age;
+    double height;
+  }
+  people::age()
+  {
+    return people::age;
+    }
+  people::height()
+  {
+  }
+  
+  
